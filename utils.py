cfa123d54e1679f595529d07de6a9eb676b23142
jaredkirby <jpeterkirby@gmail.com>
2023-06-16 15:37:14 -0700

    Key Updates

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
-from secret import OPENAI_API_KEY
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
 
-st.secrets["openai_api_key"] = OPENAI_API_KEY
-openai_api_key = OPENAI_API_KEY
+openai_api_key = st.secrets["openai_api_key"]
 chat_35_7 = ChatOpenAI(temperature=0.7, openai_api_key=openai_api_key)
