0e428f59020973abe7c3539361511b94176bbb3d
jaredkirby <jpeterkirby@gmail.com>
2023-07-11 09:55:33 -0700

    Page Config

---
+++ config.py
@@ -0,0 +1,12 @@
+# Page Configurations
+PAGE_TITLE = "PromptPilot"
+PAGE_ICON = "🛠️"
+SUB_TITLE = "Tools for Prompting LLMs"
+LAYOUT = "centered"
+
+# Model Configurations
+MODEL = [
+    "gpt-4",
+    "gpt-3.5-turbo",
+    "gpt-3.5-turbo-16k",
+]
